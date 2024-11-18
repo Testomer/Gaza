a20f68868dd8ff6fcd30be4225989cbddc2cca48
Testomer <omerauer884@gmail.com>
2024-11-18 14:45:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// عرض الجدول
+function showSchedule() {
+    const modal = document.getElementById('scheduleModal');
+    modal.style.display = 'block';
+}
+
+// إخفاء الجدول
+function hideSchedule() {
+    const modal = document.getElementById('scheduleModal');
+    modal.style.display = 'none';
+}
